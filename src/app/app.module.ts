eb1ac45022e332d21dca95a256f5bd41587246ab
Vina del Mar <vina@duoc.acad>
2023-09-22 20:29:46 -0300

    fire

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,10 +7,15 @@ import { FormsModule } from '@angular/forms'; // Importa FormsModule para el uso
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { ForgotPasswordComponent } from './forgot-password/forgot-password.component'; // Agrega la importación de ForgotPasswordComponent
+import { AngularFireModule } from '@angular/fire/compat'; // Importa AngularFireModule desde @angular/fire/compat
+import { AngularFireAuthModule } from '@angular/fire/compat/auth'; // Importa AngularfireauthModule desde @angular/fire/compat/auth
+import { environment } from 'src/environments/environment';
+
 
 @NgModule({
   declarations: [AppComponent, ForgotPasswordComponent], // Agrega ForgotPasswordComponent aquí
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule, FormsModule], // Agrega FormsModule aquí
+  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule, FormsModule, 
+  AngularFireModule, AngularFireAuthModule, AngularFireModule.initializeApp(environment.firebaseConfig)], // Agrega FormsModule aquí
   providers: [
     { provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
     GoogleMaps, // Agrega GoogleMaps si es necesario
--- src/app/register/register.page.ts
+++ src/app/register/register.page.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { AutheticationService } from '../authetication.service';
+import { Router } from '@angular/router';
+import { LoadingController, ToastController } from '@ionic/angular';
 
 @Component({
   selector: 'app-register',
@@ -6,10 +10,30 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./register.page.scss'],
 })
 export class RegisterPage implements OnInit {
+  regform: FormGroup | undefined;
 
-  constructor() { }
+  constructor(public formBuilder:FormBuilder, public loadingController:LoadingController, public toastController:ToastController, public router:Router, public authService:AutheticationService) { 
 
-  ngOnInit() {
   }
 
+  ngOnInit() {
+    this.regform = this.formBuilder.group({
+      name: ['',Validators.required],
+      email: ['',Validators.required,Validators.email,Validators.pattern('^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+.[a-zA-Z0-9-.]+$')],
+      password: ['',Validators.required,Validators.pattern('^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+.[a-zA-Z0-9-.]+$')]
+    }
+    )
+  }
+
+  get errorControl() {
+    return this.regform?.controls;
+  }
+
+  async signup() {
+    const loading = await this.loadingController.create();
+    await loading.present();
+    if(this.regform?.valid){
+     // const user = await this.authService.registerUser(this.regform?.value.email, this.regform?.value.password);
+    }
+  }
 }
