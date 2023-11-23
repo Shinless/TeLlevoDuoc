b980980e5d459be9ded7491f762d3796e74f7b2d
Shinless <ale.troncosov@duocuc.cl>
2023-11-23 15:14:20 -0300

    Flecha

--- src/app/user/user.page.ts
+++ src/app/user/user.page.ts
@@ -1,87 +1,105 @@
-import { Component, ElementRef, OnInit, Renderer2, ViewChild } from '@angular/core';
-import { UserData } from '../models/UserData';
-import { ActivatedRoute, Router } from '@angular/router';
-import { GmapsService } from '../services/gmaps/gmaps.service';
-import { Preferences } from '@capacitor/preferences';
-import { StorageService } from '../services/Storage/storage.service';
-import { ConnectionService } from '../services/connections/connection.service';
+import { Component, ElementRef, Renderer2, ViewChild } from '@angular/core';
 import { Geolocation } from '@ionic-native/geolocation/ngx';
-
+import { ConnectionService } from '../services/connections/connection.service';
+import { GmapsService } from '../services/gmaps/gmaps.service';
+import { StorageService } from '../services/Storage/storage.service';
+import { UserData } from '../models/UserData';
+import { Geoposition, PositionError } from '@ionic-native/geolocation/ngx';
 
 @Component({
   selector: 'app-user',
   templateUrl: './user.page.html',
   styleUrls: ['./user.page.scss'],
 })
-export class UserPage implements OnInit {
+export class UserPage {
   @ViewChild('map', { static: true })
-  mapElementRef!: ElementRef; // Referencia al elemento del mapa en la plantilla
-  googleMaps: any; // Objeto para el servicio de mapas de Google
-  center = { lat: -33.033667, lng: -71.533596 }; // Coordenadas del centro del mapa
-  map: any; // Objeto del mapa
-  Datos_usuario: UserData | undefined; // Datos del usuario actual
-  idUserHtmlRouterLink: any; // ID del usuario obtenido de la URL
-  id_user: any; // ID del usuario actual
-  user_name : string = '';
+  mapElementRef!: ElementRef;
+  googleMaps: any;
+  map: any;
+  Datos_usuario: UserData | undefined;
+  id_user: any;
+  user_name: string = '';
+  userMarker: any; // Variable para almacenar el marcador
 
   constructor(
-    //private router: Router,
-    //private activatedRoute: ActivatedRoute,
     private gmaps: GmapsService,
     private renderer: Renderer2,
-    private Storage: StorageService,
+    private storage: StorageService,
     private connectionService: ConnectionService,
     private geolocation: Geolocation
   ) {
-    // Obtiene los datos del usuario de la navegación y el ID de la URL
-    //this.Datos_usuario = this.router.getCurrentNavigation()?.extras.state?.['userInfoSend'];
-    //this.idUserHtmlRouterLink = this.activatedRoute.snapshot.params['id'];
-    this.Storage.obtener('IdUser').then((data) => {
+    this.storage.obtener('IdUser').then((data) => {
       this.id_user = parseInt(data?.valueOf()!);
-      console.log(this.id_user);});
-    this.Storage.obtener('NameUser').then((data) => {
-      this.user_name = data || "";
-      console.log(this.user_name);});
-  }
-
-  ngOnInit(
-
-    
-  ) {
-    // Lógica de inicialización al cargar la página
-  }
-
-  getNombreUsuario(){
-      this.connectionService.getUserById(this.id_user).subscribe(user => {
-      this.Datos_usuario = user;
-      this.user_name = this.Datos_usuario?.name;
-      console.log(this.Datos_usuario);
+      console.log(this.id_user);
+    });
+    this.storage.obtener('NameUser').then((data) => {
+      this.user_name = data || '';
+      console.log(this.user_name);
     });
   }
 
-  ngAfterViewInit() {
-    // Lógica que se ejecuta después de que los elementos de la vista se inicializan
+  ngOnInit() {
     this.loadMap();
   }
 
   async loadMap() {
     try {
+      const coordinates = await this.geolocation.getCurrentPosition();
+      const lat = coordinates.coords.latitude;
+      const lng = coordinates.coords.longitude;
 
-        const coordinates = await this.geolocation.getCurrentPosition();
-        const lat = coordinates.coords.latitude;
-        const lng = coordinates.coords.longitude;
-      // Carga el módulo de Google Maps
       let googleMaps: any = await this.gmaps.loadGoogleMaps();
       this.googleMaps = googleMaps;
-      const mapEl = this.mapElementRef.nativeElement; // Elemento del mapa en la plantilla
-      const location = new googleMaps.LatLng(lat, lng); // Ubicación del mapa
+      const mapEl = this.mapElementRef.nativeElement;
+      const location = new googleMaps.LatLng(lat, lng);
+
       this.map = new googleMaps.Map(mapEl, {
         center: location,
-        zoom: 15
-      }); // Crea el mapa
-      this.renderer.addClass(mapEl, 'visible'); // Agrega la clase 'visible' al mapa
+        zoom: 15,
+      });
+
+      this.renderer.addClass(mapEl, 'visible');
+
+      this.userMarker = new googleMaps.Marker({
+        position: location,
+        map: this.map,
+        title: 'Ubicación actual',
+        icon: {
+          url: 'assets/icon/pin.png',
+          scaledSize: new googleMaps.Size(40, 50),
+        },
+        animation: googleMaps.Animation.BOUNCE,
+      });
+
+      this.trackLocation(); // Agrega la función de seguimiento de ubicación
     } catch (e) {
       console.log('Error al cargar el mapa:', e);
+    }
+  }
+
+  trackLocation() {
+    const watchOptions: PositionOptions = {
+      enableHighAccuracy: true,
+    };
+
+    this.geolocation.watchPosition(watchOptions).subscribe(
+      (position: Geoposition | PositionError) => {
+        if ('coords' in position && position.coords) {
+          const { latitude, longitude } = position.coords;
+          this.updateMap(latitude, longitude);
+        } else if (position instanceof GeolocationPositionError) {
+          console.error('Error al obtener la ubicación:', position);
+        }
+      }
+    );
+  }
+
+  updateMap(lat: number, lng: number) {
+    if (this.map && this.userMarker) {
+      const newLocation = new this.googleMaps.LatLng(lat, lng);
+
+      this.map.panTo(newLocation);
+      this.userMarker.setPosition(newLocation);
     }
   }
 }
